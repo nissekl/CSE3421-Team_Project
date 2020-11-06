3699926f14c8bfa8cc45bc16e55568769599eb5e
qb184 <quynhbui184@gmail.com>
2020-11-05 19:45:06 -0500

    added 5d

--- Simple Queries/Q5-d.sql
+++ Simple Queries/Q5-d.sql
@@ -1,2 +1,8 @@
 /*d. Provide a list of the IP Item names and associated dollar totals for copies sold to all buyers, 
 sorted from the IP Item that has sold the highest dollar amount to the IP Item that has sold the smallest.*/
+
+SELECT      P.name,SUM(qty)*P.price AS total
+FROM        PRODUCT AS P, HAS AS H
+WHERE       P.Product_id=H.Product_id
+GROUP BY    P.product_id
+ORDER BY    SUM(H.qty)*P.price DESC;
