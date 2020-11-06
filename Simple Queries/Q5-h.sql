a0b06e62eeab78824191eec7629e5ab2a4dd103c
CadeSbrocco <44620948+CadeSbrocco@users.noreply.github.com>
2020-11-06 15:17:04 -0500

    Fixed Q5-h

--- Simple Queries/Q5-h.sql
+++ Simple Queries/Q5-h.sql
@@ -1,12 +1,13 @@
 /*h. Provide the list of sellers who listed the IP Items purchased by the buyers who have spent more than the average buyer.*/
 
-SELECT SELLER(*)
-FROM PRODUCT AS p, SELLER AS s, HAS AS h, ORDER_ AS O, (SELECT 		BUYER(*)
+SELECT s.* 
+FROM PRODUCT AS p, SELLER AS s, HAS AS h, ORDER_ AS O, (SELECT 		B.*
                                                         FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
                                                         WHERE		A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id
                                                         GROUP BY 	A.account_id
                                                         HAVING 		SUM(H.Qty*P.Price)>(SELECT 	SUM(H.Qty*P.Price)/COUNT(DISTINCT A.Account_id)
-                                                                                        FROM	ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
-                                                                                        WHERE	A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND 
-                                                                                                O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id)) as b
+                                                        FROM	ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+                                                        WHERE	A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id)) as b
+                                                                                                
+
 WHERE p.Product_id = h.Product_id AND o.Transaction_id = h.Transaction_id AND o.Account_id = b.Account_id AND p.Account_id = s.Account_id;                                                                                      
