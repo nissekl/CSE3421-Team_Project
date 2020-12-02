ecb053b42896d0dcc7303d654ed9786a856992da
CadeSbrocco <44620948+CadeSbrocco@users.noreply.github.com>
2020-12-02 15:46:45 -0500

    ACCOUNT->ACCOUNT1

--- Simple Queries/Q5-h.sql
+++ Simple Queries/Q5-h.sql
@@ -2,11 +2,11 @@
 
 SELECT s.* 
 FROM PRODUCT AS p, SELLER AS s, HAS AS h, ORDER_ AS O, (SELECT 		B.*
-                                                        FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+                                                        FROM		ACCOUNT1 AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
                                                         WHERE		A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id
                                                         GROUP BY 	A.account_id
                                                         HAVING 		SUM(H.Qty*P.Price)>(SELECT 	SUM(H.Qty*P.Price)/COUNT(DISTINCT A.Account_id)
-                                                        FROM	ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+                                                        FROM	ACCOUNT1 AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
                                                         WHERE	A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id)) as b
                                                                                                 
 
