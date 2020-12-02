1e3c8ea787c606a7fecee2379bf23621e675203f
CadeSbrocco <44620948+CadeSbrocco@users.noreply.github.com>
2020-12-02 15:41:14 -0500

    ACCOUNT->ACCOUNT1

--- Simple Queries/Q3-f.sql
+++ Simple Queries/Q3-f.sql
@@ -1,12 +1,12 @@
 /* f. Find the buyer who has purchased the most IP Items and the total number of IP Items they have purchased */
 
 SELECT A.Name,    COUNT(*)
-FROM ACCOUNT AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
+FROM ACCOUNT1 AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
 WHERE A.Account_id = ORD.Account_id AND ORD.Transaction_id = HA.Transaction_id AND HA.Product_id = PR.Product_id
 GROUP BY A.Name
 HAVING COUNT(*)=
         (SELECT MAX(BC)
         FROM (SELECT   COUNT(*) AS BC
-      	    FROM ACCOUNT AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
+      	    FROM ACCOUNT1 AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
             WHERE A.Account_id = ORD.Account_id AND ORD.Transaction_id = HA.Transaction_id AND HA.Product_id = PR.Product_id
       	    GROUP BY A.Name));
