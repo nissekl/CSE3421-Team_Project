9f7ec209280b5b9d546fb72deab33f6b432c2744
CadeSbrocco <44620948+CadeSbrocco@users.noreply.github.com>
2020-12-02 15:43:51 -0500

    ACCOUNT->ACCOUNT1

--- Simple Queries/Q5-c.sql
+++ Simple Queries/Q5-c.sql
@@ -2,7 +2,7 @@
  sorted from the IP Item that has sold the most individual copies to the IP Item that has sold the least.*/
 
 SELECT PR.Name, SUM(HA.QTY)
-FROM ACCOUNT AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
+FROM ACCOUNT1 AS A, ORDER_ AS ORD, HAS AS HA, PRODUCT AS PR
 WHERE A.Account_id = ORD.Account_id AND ORD.Transaction_id = HA.Transaction_id AND HA.Product_id = PR.Product_id
 GROUP BY PR.Product_id
 ORDER BY SUM(HA.QTY) DESC
