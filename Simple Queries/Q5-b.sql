897fafb18631d618c6c597104b6a8ba4e8fe1753
CadeSbrocco <44620948+CadeSbrocco@users.noreply.github.com>
2020-12-02 15:43:28 -0500

    ACCOUNT->ACCOUNT1

--- Simple Queries/Q5-b.sql
+++ Simple Queries/Q5-b.sql
@@ -1,10 +1,10 @@
 /* b. Provide a list of buyer names and e-mail addresses for buyers who have spent more than the average buyer. */
 
 SELECT 		A.Name,A.Email
-FROM		ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+FROM		ACCOUNT1 AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
 WHERE		A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id
 GROUP BY 	A.account_id
 HAVING 		SUM(H.Qty*P.Price)>(SELECT 	SUM(H.Qty*P.Price)/COUNT(DISTINCT A.Account_id)
-                                FROM	ACCOUNT AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
+                                FROM	ACCOUNT1 AS A, BUYER AS B, ORDER_ AS O, HAS AS H, PRODUCT AS P
                                 WHERE	A.Account_id=B.Account_id AND B.Account_id=O.Account_id AND 
                                         O.Transaction_id = H.Transaction_id AND	P.Product_id=H.Product_id);
